560378b6096209d92c869805d692f969649b878b
nimish-habile <nimish.toshniwal@habilelabs.io>
2024-07-15 11:16:49 +0530

    calculator complete

--- Python/calculator.py
+++ Python/calculator.py
@@ -1,5 +1,5 @@
 def add():
-    n = int(input("Enter the no. of numbers you want to add : "))
+    n = int(input("Enter now many numbers you want to add : "))
     L = []
     ans=0
     for i in range(1, n+1):
@@ -22,7 +22,7 @@ def sub():
     return a-b
 
 def mul():
-    n = int(input("Enter the no. of numbers you want to multiply : "))
+    n = int(input("Enter how many numbers you want to multiply : "))
     L = []
     ans=1
     for i in range(1, n+1):
@@ -39,18 +39,58 @@ def mul():
         ans*=i 
     return ans
 
-print("Enter 1 to add numbers.")
-print("Enter 2 to subtract 2 numbers.")
-print("Enter 3 to multiply numbers.")
-print("Enter 4 to divide 2 numbers.")
+def div():
+    a = int(input("Enter the first number : "))
+    b = int(input("Enter the second number : "))
+    try:
+        return a/b
+    except ZeroDivisionError:
+        print("You cannot divide by zero.")
+    except:
+        print("Some unknown error")
 
-operationType = int(input("Enter here : "))
-if operationType == 1:
-    ans = add()
-    print(f"The sum of the numbers you entered is {ans}.")
-elif operationType == 2:
-    ans = sub()
-    print(f"The subtraction of both the numbers is {ans}.")
-elif operationType == 3:
-    ans=mul()
-    print(f"The product of the numbers you entered is {ans}.")
+def exp(check):
+    x=int(input("Enter x : "))
+    n=int(input("Enter y : "))
+    if check:
+        return pow(x, n)
+    else:
+        if n==0:
+            print("Enter a number other than 0.")
+        else:
+            return pow(x, 1/n)
+
+while True:
+    try:
+        print("Enter 1 to add numbers.")
+        print("Enter 2 to subtract 2 numbers.")
+        print("Enter 3 to multiply numbers.")
+        print("Enter 4 to divide 2 numbers.")
+        print("Enter 5 to calculate x^n.")
+        print("Enter 6 to calculate x^(1/n).")
+        print("Enter anything else to exit.")
+        operationType = int(input("Enter here : "))
+        
+        if operationType == 1:
+            ans = add()
+            print(f"The sum of the numbers you entered is {ans}.")
+        elif operationType == 2:
+            ans = sub()
+            print(f"The subtraction of both the numbers is {ans}.")
+        elif operationType == 3:
+            ans=mul()
+            print(f"The product of the numbers you entered is {ans}.")
+        elif operationType == 4:
+            ans=div()
+            print(f"The result of division is {ans}.")
+        elif operationType == 5:
+            ans=exp(1)
+            print(f"x raised to power n is {ans}.")
+        elif operationType == 6:
+            ans=exp(0)
+            print(f"The value of x^(1/n) is {ans}.")
+        else:
+            break
+    except:
+        print("Some unknown error. EXIT")
+        break
